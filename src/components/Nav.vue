0f7cbe96ef8c5a915d88091c5af8f122d2b3211d
sbgrb <974487599@qq.com>
2020-08-18 21:47:23 +0800

    添加导航样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,16 @@
 <template>
-    <div class="nav">
-        <router-link to="/money">记账</router-link>
-        |
-        <router-link to="/labels">标签</router-link>
-        |
-        <router-link to="/statistics">统计</router-link>
-    </div>
+    <nav>
+        <router-link to="/money" class="item">
+            <Icon name="money" />
+            记账</router-link>
+        <router-link to="/labels" class="item">
+            <Icon name="labels"/>标签</router-link>
+        <router-link to="/statistics" class="item">
+            <Icon name="statistics" />统计</router-link>
+    </nav>
 </template>
 
-<script>
+<script lang="ts">
     export default {
         name: "Nav"
     };
@@ -16,4 +18,21 @@
 
 <style lang="scss" scoped>
     .nav{border: 1px solid red}
+    nav{
+        display: flex;
+        box-shadow: 0 0 3px rgba(0,0,0,0.3);
+        flex-direction: row;
+
+        > .item{
+            display: flex;
+            width: 33.33333%;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            .icon{
+                height: 32px;
+                width: 32px;
+            }
+        }
+    }
 </style>

09967df81483369e6130bf81ae3bf110b2510162
sbgrb <974487599@qq.com>
2020-08-19 23:47:14 +0800

    完成了Money样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,28 +1,5 @@
 <template>
-    <Layout>
-        <div class="tags">
-            <ul class="current">
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-            </ul>
-            <div class="new">
-                <button>新增标签</button>
-            </div>
-        </div>
-        <div>
-            <label class="notes">
-                <span class="name">备注</span>
-                <input type="text" placeholder="在这里输入备注">
-            </label>
-        </div>
-        <div>
-            <ul class="types">
-                <li class="selected">支出</li>
-                <li>收入</li>
-            </ul>
-        </div>
+    <Layout class-prefix="layout">
         <div class="numberPad">
             <div class="output">100</div>
             <div class="buttons">
@@ -37,10 +14,33 @@
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
-                <button>OK</button>
-                <button>0</button>
+                <button class="ok">OK</button>
+                <button class="zero">0</button>
                 <button>.</button>
             </div>
+        </div>
+        <div>
+            <ul class="types">
+                <li class="selected">支出</li>
+                <li>收入</li>
+            </ul>
+        </div>
+        <div>
+            <label class="notes">
+                <span class="name">备注</span>
+                <input type="text" placeholder="在这里输入备注">
+            </label>
+        </div>
+        <div class="tags">
+            <div class="new">
+                <button>新增标签</button>
+            </div>
+            <ul class="current">
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ul>
         </div>
     </Layout>
 </template>
@@ -50,14 +50,24 @@
         name: "Money",
     };
 </script>
-
+<style lang="scss">
+    .layout-content{
+        display: flex;
+        flex-direction: column-reverse;
+    }
+</style>
 <style lang="scss" scoped>
     @import "~@/assets/style/helper.scss";
     .tags{
         font-size: 14px;
         padding: 16px;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column-reverse;
         > .current{
             display: flex;
+            flex-wrap: wrap;
+
             > li{
                 background: #d9d9d9;
                 $h : 24px;
@@ -66,6 +76,7 @@
                 border-radius: $h/2;
                 padding: 0 16px;
                 margin-right: 12px;
+                margin-top: 4px;
             }
         }
         > .new{
@@ -115,6 +126,55 @@
                 width: 100%;
                 height: 4px;
                 background: #333333;
+            }
+        }
+    }
+    .numberPad{
+        .output{
+            font-size: 36px;
+            font-family: Consolas,monospace;
+            padding: 9px 16px;
+            text-align: right;
+            box-shadow: inset 0 -5px 5px -5px fade_out(black,0.7),
+                        inset 0 5px 5px -5px fade_out(black,0.7);
+        }
+        .buttons{
+            @extend %clearFix;
+            > button{
+                width: 25%;
+                height: 64px;
+                float: left;
+                background: transparent;
+                border: none;
+                &.ok{
+                    height: 64*2px;
+                    float: right;
+                }
+                &.zero{
+                    width: 25*2%;
+                }
+                $bg: #f2f2f2;
+                &:nth-child(1){
+                    background:$bg;
+                }
+                &:nth-child(2),&:nth-child(5){
+                    background: darken($bg,4%);
+                }
+                &:nth-child(3),&:nth-child(6),&:nth-child(9){
+                    background: darken($bg,4*2%);
+                }
+                &:nth-child(4),&:nth-child(7),&:nth-child(10){
+                    background: darken($bg,4*3%);
+                }
+                &:nth-child(8),&:nth-child(11),&:nth-child(13){
+                    background: darken($bg,4*4%);
+                }
+                &:nth-child(14){
+                    background: darken($bg,4*5%);
+                }
+                &:nth-child(12){
+                    background: gainsboro;
+                }
             }
         }
     }

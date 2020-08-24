9b68921965007ab7a93e1cc9f99ac015a236718d
sbgrb <974487599@qq.com>
2020-08-25 00:05:51 +0800

    完成Labels.vue 的创建 删除

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -3,26 +3,22 @@
         <label class="formItem">
             <span class="name">{{this.fieldName}}</span>
             <input type="text"
-                v-model = "value"
-                   :placeholder="this.placeholder">
+                :value= "value"
+                   @input="onValueChanged($event.target.value)"
+                   :placeholder="placeholder">
         </label>
     </div>
 </template>
 
 <script lang="ts">
     import Vue from 'vue'
-    import {Component, Prop, Watch} from "vue-property-decorator";
+    import {Component, Prop} from "vue-property-decorator";
 
     @Component
     export default class FormItem extends Vue{
-        value = '';
-        // onInput(event: KeyboardEvent){
-        //     const input = event.target as HTMLInputElement
-        //     this.value = input.value
-        // }
+        @Prop({default: ''}) readonly value!: string
         @Prop({required: true}) fieldName!: string
         @Prop() placeholder?: string
-        @Watch('value')
         onValueChanged(value: string)
         {this.$emit('update:value',value)}
     }
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,17 @@
 <template>
     <Layout>
         <div class="navBar">
-            <Icon class="leftIcon" name="left"/>
+            <Icon class="leftIcon" name="left" @click="goBack"/>
             <span class="title">编辑标签</span>
             <span class="rightIcon"></span>
         </div>
         <div class="form-wrapper">
-            <FormItem field-name="标签名" placeholder="请输入标签名"/>
+            <FormItem :value="tag.name"
+                      @update:value="update"
+                      field-name="标签名" placeholder="请输入标签名"/>
         </div>
         <div class="button-wrapper">
-            <Button>删除标签</Button>
+            <Button @click="remove">删除标签</Button>
         </div>
     </Layout>
 </template>
@@ -25,16 +27,35 @@
         components: {Button, FormItem}
     })
     export default class EditLabel extends Vue {
+        tag?: { id: string; name: string } = undefined;
+
         created() {
             const id = this.$route.params.id
             tagListModel.fetch();
             const tags = tagListModel.data;
             const tag = tags.filter(t => t.id === id)[0];
             if (tag) {
-                console.log(tag);
+                this.tag = tag
             } else {
                 this.$router.replace('/404')
             }
+        }
+
+        update(name: string) {
+            if (this.tag) {
+                tagListModel.update(this.tag.id, name)
+            }
+        }
+
+        remove() {
+            if (this.tag) {
+                if(tagListModel.remove(this.tag.id)){
+                    this.$router.back()
+                }
+            }
+        }
+        goBack(){
+            this.$router.back()
         }
     }
 </script>
@@ -48,6 +69,7 @@
         display: flex;
         align-items: center;
         justify-content: space-between;
+
         > .title {
 
         }
@@ -56,18 +78,21 @@
             width: 24px;
             height: 24px;
         }
-        > .rightIcon{
+
+        > .rightIcon {
             width: 24px;
             height: 24px;
         }
     }
-    .form-wrapper{
+
+    .form-wrapper {
         background: white;
         margin-top: 8px;
     }
-    .button-wrapper{
+
+    .button-wrapper {
         text-align: center;
-        padding: 16px ;
+        padding: 16px;
         margin-top: 28px;
     }
 </style>

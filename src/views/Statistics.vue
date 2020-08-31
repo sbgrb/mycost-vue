21a50876af4965295ba405cec74ffb03e7399abb
sbgrb <974487599@qq.com>
2020-08-31 23:59:41 +0800

    完成统计页面的样式 与 汇总

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,41 +1,127 @@
 <template>
     <Layout>
         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
-        <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+        <ol>
+            <li v-for="(group,index) in groupedList" :key="index">
+                <h3 class="title">{{beautify(group.title)}}</h3>
+                <ol>
+                    <li v-for="item in group.items" :key="item.id"
+                        class="record">
+                        <span>{{tagString(item.tags)}}</span>
+                        <span class="notes">{{item.notes}}</span>
+                        <span>￥{{item.amount}}</span>
+                    </li>
+                </ol>
+            </li>
+        </ol>
     </Layout>
 </template>
 
+<style scoped lang="scss">
+    %item {
+        padding: 8px 16px;
+        line-height: 24px;
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+    }
+
+    .title {
+        @extend %item;
+    }
+
+    .record {
+        @extend %item;
+        background: white;
+    }
+
+    .notes {
+        margin-right: auto;
+        margin-left: 8px;
+        color: #999999;
+    }
+</style>
 <script lang="ts">
     import Vue from 'vue'
     import {Component} from "vue-property-decorator";
     import Tabs from "@/components/Tabs.vue";
-    import intervalList from "@/consts/intervalList";
     import typeList from "@/consts/typeList";
+    import dayjs from "dayjs";
+    import clone from "@/lib/clone";
 
     @Component(
         {components: {Tabs}}
     )
     export default class Statistics extends Vue {
+        beautify(string: string) {
+            const day = dayjs(string);
+            const now = dayjs();
+            if (day.isSame(now, 'day')) {
+                return '今天';
+            } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+                return '昨天';
+            } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+                return '前天';
+            } else if (day.isSame(now, 'year')) {
+                return day.format('M月D日')
+            } else {
+                return day.format('YYYY年M月D日')
+            }
+        }
+
+        tagString(tags: Tag[]) {
+            return tags.length === 0 ? '无' : tags.join(',')
+        }
+
+        get recordList() {
+            return (this.$store.state as RootState).recordList
+        }
+
+        get groupedList() {
+            const {recordList} = this;
+            if (recordList.length === 0) {
+                return []
+            }
+            // const hashTable: { title: string; items: RecordItem[] }[]
+            const newList = clone(recordList).sort((a, b) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
+            const result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}]
+            for (let i = 1; i < newList.length; i++) {
+                const current = newList[i]
+                const last = result[result.length - 1]
+                if (dayjs(last.title).isSame(dayjs(current.createdAt), 'day')) {
+                    last.items.push(current)
+                } else {
+                    result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'), items: [current]})
+                }
+            }
+            return result
+        }
+
+        beforeCreate() {
+            this.$store.commit('fetchRecords')
+        }
+
         type = '-';
-        interval = 'day';
-        intervalList = intervalList
         recordTypeList = typeList;
     }
 </script>
 
 <style scoped lang="scss">
-    ::v-deep{
+    ::v-deep {
         .type-tabs-item {
-            background: white;
+
+            background: #c4c4c4;
 
             &.selected {
-                background: #c4c4c4;
+                background: white;
+
                 &::after {
                     display: none;
                 }
             }
         }
-        .interval-tabs-item{
+
+        .interval-tabs-item {
             height: 48px;
         }
     }
